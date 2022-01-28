c20ce820bb5c6ea2672f0ba8db2ac45899961f24
pravin desai <pravindesai100@gmail.com>
2022-01-28 21:14:48 +0530

    t

--- app/src/main/java/com/pravin/barcodeapp/mynotes/OnNoteListner.kt
+++
@@ -1,9 +0,0 @@
-package com.pravin.barcodeapp.mynotes
-
-import com.pravin.barcodeapp.mynotes.room.Note
-
-interface OnNoteListner {
-    fun onNoteDeleteClicked(note:Note, pos:Int)
-    fun onNoteClicked(note:Note, pos:Int)
-
-}
